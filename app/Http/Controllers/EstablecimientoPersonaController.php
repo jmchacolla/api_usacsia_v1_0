06941a838c82c8d8dfdd7b226a117dd2c23c0384
vero <vronk_tcviral@hotmail.com>
2018-01-11 15:25:31 -0300

    mensaje

--- app/Http/Controllers/EstablecimientoPersonaController.php
+++ app/Http/Controllers/EstablecimientoPersonaController.php
@@ -6,17 +6,40 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Http\Requests;
 use App\Models\Establecimiento_persona;
+use App\Models\PersonaTramite;
 
 
 class EstablecimientoPersonaController extends Controller
 {
     public function index($ess_id)
     {
-        $personas_x_establecimiento= Establecimiento_persona::select('ep_id','persona.per_ci','persona.per_ci_expedido','per_nombres','per_apellido_primero','per_apellido_segundo','ep_inicio_trabajo','ep_fin_trabajo','ep_cargo','ep_estado_laboral')
+        $personas_x_establecimiento= Establecimiento_persona::select('ep_id','per_id','persona.per_ci','persona.per_ci_expedido','per_nombres','per_apellido_primero','per_apellido_segundo','ep_inicio_trabajo','ep_fin_trabajo','ep_cargo','ep_estado_laboral')
         ->join('persona','persona.per_id','=','establecimiento_persona.per_id')
         ->where('establecimiento_persona.ess_id',$ess_id)
         ->orderby('ep_id','asc')
         ->get();
+
+        // foreach ($personas_x_establecimiento  as $value) {
+        //         $datos_ultimo_tramite= PersonaTramite::all()
+        //         ->where('per_id', $value->per_id)
+        //         ->orderby('created_at','desc')
+        //         ->first();
+
+        //         if($datos_ultimo_tramite->et_estado_tramite=''){
+                        
+        //         }
+        //     }
+
+       
+
+        // $iniciados=;
+        // $concluidos=;
+        // $observados=;
+        // $aprobados=;
+        // $vencidos=;
+        // $pt_estado_tramite=;
+
+
          return response()->json(['status'=>'ok','mensaje'=>'exito','personas_x_establecimiento'=>$personas_x_establecimiento],200); 
     }
 
